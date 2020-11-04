8160884b5bea42b5d8fbccb61edc68e790e778e8
lizzie friedman <alizafriedman123@gmail.com>
2020-11-04 10:58:33 -0500

    misc

--- src/react-auth0-spa.js
+++ src/react-auth0-spa.js
@@ -45,7 +45,7 @@ export const Auth0Provider = ({
         const user = await auth0FromHook.getUser();
         
         let token = await auth0FromHook.getTokenSilently();
-      
+        console.log(token)
         const res = await fetch(`${api}/users`, {
           method: "",
           body: JSON.stringify({ nickname: user.nickname, email: user.email }),
